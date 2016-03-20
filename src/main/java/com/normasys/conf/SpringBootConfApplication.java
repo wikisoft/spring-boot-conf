0107f07aa90de4053b81c01bf16099db08fdfee3
wikisoft <wikicenter-soft@yahoo.fr>
2016-03-20 20:17:28 +0100

    initial

---
+++ src/main/java/com/normasys/conf/SpringBootConfApplication.java
@@ -0,0 +1,12 @@
+package com.normasys.conf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConfApplication.class, args);
+	}
+}
---
+++ src/test/java/com/normasys/conf/SpringBootConfApplicationTests.java
@@ -0,0 +1,18 @@
+package com.normasys.conf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringBootConfApplication.class)
+@WebAppConfiguration
+public class SpringBootConfApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
